6568a04c40d04681796020dbe92ca867b8d6e680
lucianticrea <lucainticrea@gmail.com>
2012-10-12 16:21:24 +0300

    d

--- Hello Endava/Form1.cs
+++ Hello Endava/Form1.cs
@@ -19,6 +19,7 @@ namespace Hello_Endava
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Hello Endava!");
+            MessageBox.Show("Hello changes!");
         }
     }
 }
